0266e215e8446227bc72af01b2e152a6b1c8fc5f
Mereyda <mereyda2487@gmail.com>
2023-06-30 12:55:53 -0300

    actualizando

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,42 +1,69 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const Form = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [error, setError] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
 
   const handleSubmit = (e) => {
-    e.preventDefault();
 
-    if (name.length < 5 && !email.includes('@')) {
-      setError('Por favor verifique su información nuevamente');
+    e.preventDefault();
+    if(name.length < 5){
+      // setError('el nombre debe ser mayor a 5 caracteres')
+      // return;
+      setError('Solo puedes ingresar letras, con un mínimo de 5 dígitos.');
+      setTimeout(() => {
+        setError('');
+      }, 3000);
+      return;
+    } else if (name.length < 5 || !email.includes("@")) {
+      setError("Por favor verifique su información nuevamente");
       return;
     }
 
-    setSuccessMessage(`Gracias ${name}, te contactaremos lo antes posible vía email`);
+    setSuccessMessage(
+      `Gracias ${name}, te contactaremos lo antes posible vía email a ${email}`
+    );
+    setError('')
   };
 
-  return (
-      <form onSubmit={handleSubmit} className='form'>
-        <label>
-          Nombre completo:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        </label>
-        <br />
-        <br />
+  useEffect(()=>{
+    if(successMessage){
+      const msj = setTimeout(() => {
+        setSuccessMessage('')
 
-        <label>
-          Email:
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <br />
-        <br />
-        <button type="submit">Enviar</button>
-        {error && <p>{error}</p>}
+      }, 3000);
+      return () => clearTimeout(msj)
+    }
+  },[successMessage])
+
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="ingresa tu nombre"
+      />
+
+      <br />
+      <br />
+
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="ingresa tu Email"
+      />
+
+      <br />
+      <br />
+      <button type="submit">Enviar</button>
+      {error && <p>{error}</p>}
       {successMessage && <p>{successMessage}</p>}
-      </form>
-     
+    </form>
   );
 };
 
